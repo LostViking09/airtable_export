554c42329bb945f4cbbb1f8b289e70dba4c7ab9a
LostViking09 <117819350+LostViking09@users.noreply.github.com>
2026-06-02 11:48:29 +0200

    feat: add gh-pages deployment scripts and configuration to package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/airtable_export/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
